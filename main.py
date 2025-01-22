7a2f6d1a8f4e1ce0ee8be1020b8b17cf2c269543
RondeBoB <morgengaikvissenvangen@hotmail.com>
2025-01-22 08:40:36 +0100

    003 Updated main.pyu with task L9

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def read_book():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        return(file_contents)
+
+def count_words():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = len(file_contents.split())
+        print(words)
+    
+count_words()
